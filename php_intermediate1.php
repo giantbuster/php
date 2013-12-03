d1150ae2f147b52ca46266daa7be4b35c353173b
Jefferson Lam <jlam520@gmail.com>
2013-12-03 10:10:02 -0800

    first commit

---
+++ php_intermediate1.php
@@ -0,0 +1,31 @@
+<!-- 
+	Jefferson Lam
+	12-3-13
+	PHP Intermediate 1
+-->
+
+<?php
+	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Intermediate 1</title>
+	<meta charset='UTF-8'>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<style>
+		*{
+			font-family: verdana;
+		}
+	</style>
+</head>
+<body>
+<div class="container">
+	<?php 
+		
+	?>
+</div>
+</body>
+</html>
