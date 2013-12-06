fbb1f5b6ad8e7924b2d15144d055f10dd7cd1ebb
Jefferson Lam <jlam520@gmail.com>
2013-12-06 14:05:26 -0800

    Started on Advanced hw

---
+++ advanced_1/index.php
@@ -0,0 +1,22 @@
+<?php
+	session_start();
+?>
+
+<!--
+	Jefferson Lam
+	12-6-13
+	Advanced PHP : Advanced : Registration Page
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Advanced PHP : Advanced : Registration Page</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+</head>
+<body>
+	
+</body>
+</html>
