e66c7732cd7aeea536433f1ba10d069bec753527
Jefferson Lam <jlam520@gmail.com>
2013-12-06 09:19:59 -0800

    started intermediate_1, barebones skeleton

---
+++ intermediate_1/index.php
@@ -0,0 +1,32 @@
+<?php
+	session_start();
+?>
+
+
+<!--
+
+	Jefferson Lam
+	12-6-13
+	Advanced PHP: Intermediate : Ninja Gold Game
+
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Advanced PHP: Intermediate : Ninja Gold Game</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<link rel="stylesheet" type="text/css" href="global.css">
+</head>
+<body>
+<div class="container">
+
+</div>
+</body>
+</html>
+
+<?php
+	$_SESSION = array();
+?>
