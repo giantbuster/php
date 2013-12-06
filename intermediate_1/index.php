017ab86e03025a7ad8ea8ffdbde2895a9e2aa242
Jefferson Lam <jlam520@gmail.com>
2013-12-06 13:16:38 -0800

    Finished ninja gold game: finished HTML structure, CSS style, and PHP functionality. Can find gold from four sources, prints logs of each action with place/amount/date, colors green or red based on gold result. Also implemented reset button.

--- intermediate_1/index.php
+++ intermediate_1/index.php
@@ -1,7 +1,10 @@
 <?php
 	session_start();
-?>
 
+	if (isset($_SESSION['reset'])){
+		$_SESSION = array();
+	}	
+?>
 
 <!--
 
@@ -22,11 +25,84 @@
 </head>
 <body>
 <div class="container">
-
+	<div class="status">
+		<div>
+			<label>Your Gold:</label>
+			<div class="gold-amount"><span>
+				<?php
+					if (isset($_SESSION['feedback'])){
+						$sum = 0;
+						foreach($_SESSION['feedback']['gold'] as $value){
+							$sum += $value;
+						}
+						echo $sum;
+					} else {
+						echo 0;
+					}
+				?>
+			</span></div>
+		</div>
+	</div>
+	<div class="destinations">
+		<div class="col farm">
+			<h3>Farm</h3>
+			<h4>(earns 10-20 golds)</h4>
+			<form action="process.php" method="post">
+				<input type="hidden" name="action" value="farm">
+				<input type="submit" value="Find Gold!">
+			</form>
+		</div>
+		<div class="col cave">
+			<h3>Cave</h3>
+			<h4>(earns 5-10 golds)</h4>
+			<form action="process.php" method="post">
+				<input type="hidden" name="action" value="cave">
+				<input type="submit" value="Find Gold!">
+			</form>
+		</div>
+		<div class="col house">
+			<h3>House</h3>
+			<h4>(earns 2-5 golds)</h4>
+			<form action="process.php" method="post">
+				<input type="hidden" name="action" value="house">
+				<input type="submit" value="Find Gold!">
+			</form>
+		</div>
+		<div class="col casino">
+			<h3>Casino!</h3>
+			<h4>(earns/takes 0-50 golds)</h4>
+			<form action="process.php" method="post">
+				<input type="hidden" name="action" value="casino">
+				<input type="submit" value="Find Gold!">
+			</form>
+		</div>
+	</div>
+	<div class="footer">
+		<label>Activities:</label>
+		<div class="feedback">
+			<?php 
+				if (isset($_SESSION['feedback'])){
+					// var_dump($_SESSION);
+					for($i = count($_SESSION['feedback']['gold'])-1; $i>=0; $i--){
+						$place = $_SESSION['feedback']['place'][$i];
+						$gold = $_SESSION['feedback']['gold'][$i];
+						$time = $_SESSION['feedback']['time'][$i];
+						if ($gold >= 0){
+							echo '<p class="earned">You entered a '.$place.' and earned '.$gold.' golds. ('.$time.')</p>';
+						} else {
+							echo '<p class="lost">You entered a '.$place.' and lost '.$gold.' golds... Ouch... ('.$time.')</p>';
+						}
+					}
+				}
+			?>
+		</div>
+		<div class="reset-form">
+			<form action="process.php" method="post">
+				<input type="hidden" name="action" value="reset">
+				<input type="submit" value="Reset">
+			</form>
+		</div>
+	</div>
 </div>
 </body>
 </html>
-
-<?php
-	$_SESSION = array();
-?>
--- intermediate_1/process.php
+++ intermediate_1/process.php
@@ -1,4 +1,48 @@
 <?php
 	session_start();
-	
+	function farm(){
+		return rand(10, 20);
+	}
+
+	function cave(){
+		return rand(5, 10);
+	}
+
+	function house(){
+		return rand(2, 5);
+	}
+
+	function casino(){
+		return rand(-50, 50);
+	}
+
+
+	if(isset($_POST['action'])){
+		switch ($_POST['action']){
+			case 'farm':
+				$_SESSION['feedback']['gold'][] = farm();
+				$_SESSION['feedback']['place'][] = 'farm';
+				break;
+			case 'cave':
+				$_SESSION['feedback']['gold'][] = cave();
+				$_SESSION['feedback']['place'][] = 'cave';
+				break;
+			case 'house':
+				$_SESSION['feedback']['gold'][] = house();
+				$_SESSION['feedback']['place'][] = 'house';
+				break;
+			case 'casino':
+				$_SESSION['feedback']['gold'][] = casino();
+				$_SESSION['feedback']['place'][] = 'casino';
+				break;
+			case 'reset':
+				$_SESSION['reset'] = true;
+				header('Location: index.php');
+			}
+		$date = new DateTime('now', new DateTimeZone('US/Pacific'));
+ 		$_SESSION['feedback']['time'][] = $date->format('F dS Y H:i a');
+	}
+
+	header('Location: index.php');
+	exit;
 ?>
