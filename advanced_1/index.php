0bc4f2be220c969bdc9529aed9aaf75245229223
Jefferson Lam <jlam520@gmail.com>
2013-12-06 17:40:27 -0800

    Finished advanced form validation. Check all fields extensively. Also save values if user submits form but fields are not successfully validated, so user does not need to re-enter all the values every time.

--- advanced_1/index.php
+++ advanced_1/index.php
@@ -1,5 +1,28 @@
 <?php
 	session_start();
+
+	function validate_field($field){
+		if (isset($_SESSION['error'][$field])){
+			echo '<div class="error-msg">';
+		    echo '<p>'.$_SESSION['error'][$field].'</p>'; 
+		    echo '</div>';
+		}
+	}
+
+	function fill($key){
+		if (isset($_SESSION['input'][$key])){
+			echo $_SESSION['input'][$key];
+		}
+	}
+	// var_dump($_SESSION);
+
+	function fill_password($key){
+		if (isset($_SESSION['success']['password']) && isset($_SESSION['success']['confirm'])) {
+			echo $_SESSION['success'][$key];	
+		}
+	}
+
+	if (isset($_SESSION['validated'])) $_SESSION = array();
 ?>
 
 <!--
@@ -15,8 +38,65 @@
 	<title>Advanced PHP : Advanced : Registration Page</title>
 	<meta name="description" content="">
 	<meta name="keywords" content="">
+	<link rel="stylesheet" type="text/css" href="global.css">
 </head>
 <body>
-	
+<div class="container">
+	<div class="modal">
+		<form action="process.php" method="post" enctype="multipart/form-data">
+			<input type="hidden" name="action" value="registration">
+
+			<div class="row">
+				<?php validate_field('email') ?>
+				<div class="col"><label>Email</label></div>
+				<div class="col"><input type="text" name="email" placeholder="youremail@example.com" value="<?= fill('email')?>"></div>
+			</div>
+
+			<div class="row">
+				<?php validate_field('first_name') ?>
+				<div class="col"><label>First Name</label></div>
+				<div class="col"><input type="text" name="first_name" placeholder="Michael" value="<?= fill('first_name')?>"></div>
+			</div>
+
+			<div class="row">
+				<?php validate_field('last_name') ?>
+				<div class="col"><label>Last Name</label></div>
+				<div class="col"><input type="text" name="last_name" placeholder="Choi" value="<?= fill('last_name')?>"></div>
+			</div>
+
+			<div class="row">
+				<?php validate_field('password') ?>
+				<div class="col"><label>Password</label></div>
+				<div class="col"><input type="password" name="password" placeholder="Password" value="<?= fill_password('password')?>"></div>
+			</div>
+
+			<div class="row">
+				<?php validate_field('confirm') ?>
+				<div class="col"><label>Confirm Password</label></div>
+				<div class="col"><input type="password" name="confirm" placeholder="Re-enter password" value="<?= fill_password('confirm')?>"></div>
+			</div>
+
+			<div class="row">
+				<?php validate_field('dob') ?>
+				<div class="col"><label>Birth Date</label></div>
+				<div class="col"><input type="text" name="dob" placeholder="MM/DD/YYYY" value="<?= fill('dob')?>"></div>
+			</div>
+
+			<div class="row">
+			<?php validate_field('file') ?>
+				<div class="col"><label>Profile Picture</label></div>
+				<div class="col"><input type="file" name="file"></div>
+			</div>
+
+			<div class="row">
+				<div class="submit-btn"><input type="submit" value="Submit"></div>
+			</div>
+		</form>
+		<form action="process.php" method="post">
+			<input type="hidden" name="reset" value="reset">
+			<input type="submit" value="Reset">
+		</form>
+	</div>
+</div>
 </body>
 </html>
--- advanced_1/process.php
+++ advanced_1/process.php
@@ -1,4 +1,93 @@
 <?php
 	session_start();
-	
+
+	if (isset($_POST['action']) && $_POST['action'] == 'registration'){
+		//Check each of the input fields
+		foreach ($_POST as $key => $value){
+			$_SESSION['input'][$key] = $value;
+			if (empty($value)){
+				$_SESSION['error'][$key] = 'cannot be empty';
+				continue;
+			}
+			//Parse inputted values
+			switch($key){
+				case 'email':
+					if (!filter_var($value, FILTER_VALIDATE_EMAIL))
+						$_SESSION['error'][$key] = 'invalid email';
+					else {
+						$_SESSION['success'][$key] = $value;
+						unset($_SESSION['error'][$key]);
+					}
+					break;
+				case 'first_name':
+				 	if (!ctype_alpha($value)) 
+				 		$_SESSION['error'][$key] = 'Name must only contain letters';
+				 	else {
+				 		$_SESSION['success'][$key] = $value;
+				 		unset($_SESSION['error'][$key]);
+				 	}
+					break;
+				case 'last_name':
+					if (!ctype_alpha($value)) 
+						$_SESSION['error'][$key] = 'Name must only contain letters';
+					else {
+						$_SESSION['success'][$key] = $value;
+						unset($_SESSION['error'][$key]);
+					}
+					break;
+				case 'password':
+					if (strlen($value)<5) {
+						$_SESSION['error'][$key] = 'Password must be at least 6 characters';
+						unset($_SESSION['success'][$key]);
+					} else {
+						$_SESSION['success'][$key] = $value;
+						unset($_SESSION['error'][$key]);
+					}
+					break;
+				case 'confirm':
+					if (isset($_SESSION['success']['password']) && $_SESSION['success']['password'] != $value){
+						$_SESSION['error'][$key] = 'Password does not match';
+						unset($_SESSION['success'][$key]);
+					} else {
+						$_SESSION['success'][$key] = $value;
+						unset($_SESSION['error'][$key]);
+					}
+					break;
+				case 'dob':
+					$dob = explode('/', $value);
+					if (count($dob)!=3 || 
+						!is_numeric($dob[0]) || !is_numeric($dob[1]) || !is_numeric($dob[2]) || 
+						!checkdate($dob[0], $dob[1], $dob[2]))
+						$_SESSION['error'][$key] = 'Invalid date';
+					else {
+						$_SESSION['success'][$key] = $value;
+						unset($_SESSION['error'][$key]);
+					}
+					break;
+			}//end switch
+		} //end foreach loop
+
+		//Try to upload file
+		if ($_FILES['file']['error'] > 0){
+			$_SESSION['error']['file'] = "Error on file upload Return Code: ".$_FILES['file']['error'];
+		} else {
+			$directory = 'upload/';
+			$file_name = $_FILES['file']['name'];
+			$file_path = $directory.$file_name;
+			if(file_exists($file_path)){
+				$_SESSION['error']['file'] = $file_name.' already exists';
+			} else {
+				//var_dump($_FILES);
+				if (!move_uploaded_file($_FILES['file']['tmp_name'], $file_path)){
+					$_SESSION['error']['file'] = $file_name." could not be saved";
+				}
+			}
+		}
+
+		//Check if validation was successful.
+		if (!isset($_SESSION['error'])) $_SESSION['validated'] = true;
+	}
+
+	if (isset($_POST['reset'])) $_SESSION = array();
+	header('Location: index.php');
 ?>
