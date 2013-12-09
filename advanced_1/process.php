43c58bc55c200bfa9d80620192ba1a617bcafe99
Jefferson Lam <jlam520@gmail.com>
2013-12-09 09:27:57 -0800

    Edited error handling to check edge cases of passwords. Cleaned up code in switch statements to be less redundant.

--- advanced_1/process.php
+++ advanced_1/process.php
@@ -2,72 +2,59 @@
 	session_start();
 
 	if (isset($_POST['action']) && $_POST['action'] == 'registration'){
-		//Check each of the input fields
+		//Reset errors
+		unset($_SESSION['error']);
+
+		//Validate each input fields
 		foreach ($_POST as $key => $value){
 			$_SESSION['input'][$key] = $value;
-			if (empty($value)){
-				$_SESSION['error'][$key] = 'cannot be empty';
-
-				//Unset so passwords don't get filled in again
-				// unset($_SESSION['success'][$key]);
+			//If it's blank, return error
+			if (empty($value)){	
+				if ($key == 'password'){
+					$_SESSION['error']['password'] = 'Please enter password';
+				} else if ($key == 'confirm' && isset($_SESSION['success']['password'])){
+					$_SESSION['error']['password'] = '';
+					$_SESSION['error']['confirm'] = 'Please confirm password';
+				} else {
+					$_SESSION['error'][$key] = 'Cannot be empty';
+				}
 				continue;
 			}
 			//Parse inputted values
 			switch($key){
 				case 'email':
-					if (!filter_var($value, FILTER_VALIDATE_EMAIL))
-						$_SESSION['error'][$key] = 'invalid email';
-					else {
-						$_SESSION['success'][$key] = $value;
-						unset($_SESSION['error'][$key]);
-					}
+					if (!filter_var($value, FILTER_VALIDATE_EMAIL)) $_SESSION['error'][$key] = 'invalid email';
 					break;
 				case 'first_name':
-				 	if (!ctype_alpha($value)) 
-				 		$_SESSION['error'][$key] = 'Name must only contain letters';
-				 	else {
-				 		$_SESSION['success'][$key] = $value;
-				 		unset($_SESSION['error'][$key]);
-				 	}
+				 	if (!ctype_alpha($value)) $_SESSION['error'][$key] = 'Name must only contain letters';
 					break;
 				case 'last_name':
-					if (!ctype_alpha($value)) 
-						$_SESSION['error'][$key] = 'Name must only contain letters';
-					else {
-						$_SESSION['success'][$key] = $value;
-						unset($_SESSION['error'][$key]);
-					}
+					if (!ctype_alpha($value)) $_SESSION['error'][$key] = 'Name must only contain letters';
 					break;
 				case 'password':
 					if (strlen($value)<6) {
 						$_SESSION['error'][$key] = 'Password must be at least 6 characters';
 						unset($_SESSION['success'][$key]);
-					} else {
-						$_SESSION['success'][$key] = $value;
-						unset($_SESSION['error'][$key]);
-					}
+					} 
 					break;
 				case 'confirm':
 					if (isset($_SESSION['success']['password']) && $_SESSION['success']['password'] != $value){
 						$_SESSION['error'][$key] = 'Password does not match';
 						unset($_SESSION['success'][$key]);
-					} else {
-						$_SESSION['success'][$key] = $value;
-						unset($_SESSION['error'][$key]);
-					}
+					} 
 					break;
 				case 'dob':
 					$dob = explode('/', $value);
 					if (count($dob)!=3 || 
 						!is_numeric($dob[0]) || !is_numeric($dob[1]) || !is_numeric($dob[2]) || 
-						!checkdate($dob[0], $dob[1], $dob[2]))
+						!checkdate($dob[0], $dob[1], $dob[2])){
 						$_SESSION['error'][$key] = 'Invalid date';
-					else {
-						$_SESSION['success'][$key] = $value;
-						unset($_SESSION['error'][$key]);
-					}
+					} 
 					break;
 			}//end switch
+
+			//If key has no error, it's been validated, so store it as success
+			if (!isset($_SESSION['error'][$key])) $_SESSION['success'][$key] = $value;
 		} //end foreach loop
 
 		//Validate file upload
