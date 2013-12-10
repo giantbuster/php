bb31717f0de2f8f6152c3cd8ca7d6dac9fc756c4
Jefferson Lam <jlam520@gmail.com>
2013-12-10 15:20:07 -0800

    Added functionality to account for null returns from database query

--- intermediate_2/connection.php
+++ intermediate_2/connection.php
@@ -1,34 +1,40 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_DATABASE', 'intermediate_2');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', 'root');
+	define('DB_DATABASE', 'intermediate_2');
 
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);
+	$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);
 
-if(mysqli_connect_errno()){
-	echo "error connecting to database:<br>";
-	echo mysqli_connect_errno();
-}
-
-//fetches all records from the query and returns an array with the fetched records
-function fetchAll($connection, $query){
-	$data = array();
-
-	$result = mysqli_query($connection, $query);
-	while($row = mysqli_fetch_assoc($result))
-	{
-		$data[] = $row;
+	if(mysqli_connect_errno()){
+		echo "error connecting to database:<br>";
+		echo mysqli_connect_errno();
 	}
 
-	return $data;
-}
+	//fetches all records from the query and returns an array with the fetched records
+	function fetchAll($connection, $query){
+		$data = array();
 
-//fetch the first record obtained from the query
-function fetchRecord($connection, $query){
-	$result = mysqli_query($connection, $query);
-	return mysqli_fetch_assoc($result);
-}
+		$result = mysqli_query($connection, $query);
+		while($row = mysqli_fetch_assoc($result))
+		{
+			$data[] = $row;
+		}
+
+		return $data;
+	}
+
+	//fetchRecord()
+	//-------------
+	//Fetches the first record obtained from the query.
+	//Either returns the result of the query, or null if no data was received. 
+	function fetchRecord($connection, $query){
+		$result = mysqli_query($connection, $query);
+		if ($result == NULL){
+			return NULL;
+		}
+		return mysqli_fetch_assoc($result);
+	}
 
 ?>
